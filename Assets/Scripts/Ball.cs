a3136628f30b3b0db6c1f34b380175ce08baad43
clusson <clementlusson@gmail.com>
2017-09-08 11:47:14 +0200

    8 - 202

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,6 +6,7 @@ public class Ball : MonoBehaviour {
 	public Vector3 launchVelocity;
 	public bool inPlay = false;
 
+	private Vector3 ballStartPos;
 	private Rigidbody rigidBody;
 	private AudioSource audioSource;
 
@@ -14,6 +15,7 @@ public class Ball : MonoBehaviour {
 	void Start () {
 		rigidBody= GetComponent<Rigidbody>();
 		rigidBody.useGravity = false;
+		ballStartPos = transform.position;
 	}
 
 	public void Launch(Vector3 velocity) {
@@ -24,6 +26,14 @@ public class Ball : MonoBehaviour {
 
 		audioSource = GetComponent<AudioSource>();
 		audioSource.Play();
+	}
+
+	public void Reset(){
+		Debug.Log("Resetting ball");
+		inPlay = false;
+		transform.position = ballStartPos;
+		rigidBody.velocity = Vector3.zero;
+		rigidBody.angularVelocity = Vector3.zero;
 	}
 	// Update is called once per frame
 	void Update () {
--- Assets/Scripts/PinSetter.cs
+++ Assets/Scripts/PinSetter.cs
@@ -6,16 +6,49 @@ using UnityEngine.UI;
 
 public class PinSetter : MonoBehaviour {
 
+	public int lastStandingCount = -1;
 	public Text standingDisplay;
+	
+	private Ball ball;
+	private float lastChangeTime;
+	private bool ballEnteredBox = false;
 
 	// Use this for initialization
 	void Start () {
-		
+		ball = GameObject.FindObjectOfType<Ball>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		standingDisplay.text = CountStanding().ToString();
+
+		if (ballEnteredBox) {
+			CheckStanding();
+		}
+	}
+
+	void CheckStanding(){
+		//Update the lastStandingCount
+		//Call PinsHaveSettled() when they have
+		int currentStanding = CountStanding();
+
+		if (currentStanding != lastStandingCount) {
+			lastChangeTime = Time.time;
+			lastStandingCount = currentStanding;
+			return;
+		}
+
+		float settleTime = 3f; // How long to wait to consider pins settled
+		if ((Time.time - lastChangeTime) > settleTime) { //If last change > 3s ago
+			PinsHaveSettled();
+		}
+	}
+
+	void PinsHaveSettled() {
+		ball.Reset();
+		lastStandingCount = -1; //Indicates pins have settled, and ball not back in box
+		ballEnteredBox = false;
+		standingDisplay.color = Color.green;
 	}
 
 	int CountStanding() {
@@ -28,5 +61,23 @@ public class PinSetter : MonoBehaviour {
 		}
 
 		return standing;
+	}
+
+	void OnTriggerExit(Collider collider) {
+		GameObject thingLeft = collider.gameObject;
+
+		if (thingLeft.GetComponent<Pin>()){
+			Destroy(thingLeft);
+		}
+	}
+	void OnTriggerEnter (Collider collider){
+		GameObject thingHit = collider.gameObject;
+		
+		//Ball enters play box
+
+		if (thingHit.GetComponent<Ball>()) {
+			ballEnteredBox = true;
+			standingDisplay.color = Color.red;
+		}
 	}
 }
