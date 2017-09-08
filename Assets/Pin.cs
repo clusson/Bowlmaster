9209f43fbbbc27046995d5ce77715c282e37d84f
clusson <clementlusson@gmail.com>
2017-09-08 10:17:03 +0200

    8 - 198

---
+++ Assets/Pin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+	public float standingTreshold = 3f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (name + IsStanding());
+	}
+
+	public bool IsStanding () {
+		Vector3 rotationInEuler =  transform.rotation.eulerAngles;
+
+		float tiltInX = Mathf.Abs(rotationInEuler.x);
+		float tiltInZ = Mathf.Abs(rotationInEuler.z);
+
+		if (tiltInX < standingTreshold && tiltInZ < standingTreshold){
+			return true;
+		} else {
+			return false;
+		}
+
+		return true;
+	}
+}
