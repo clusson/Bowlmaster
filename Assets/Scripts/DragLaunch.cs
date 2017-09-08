79d269d586cd2f21ee9707e6218123c43e5bcf0c
clusson <clementlusson@gmail.com>
2017-09-08 09:46:34 +0200

    8 - 197 (Swiper animation)

--- Assets/Scripts/DragLaunch.cs
+++ Assets/Scripts/DragLaunch.cs
@@ -16,7 +16,10 @@ private Ball ball;
 	
 	public void MoveStart (float amount) {
 		if (!ball.inPlay){
-			ball.transform.Translate (new Vector3(amount, 0, 0), Space.World);
+			float xPos = Mathf.Clamp(ball.transform.position.x + amount, -50f, 50f);
+			float yPos = ball.transform.position.y;
+			float zPos = ball.transform.position.z;
+			ball.transform.position = new Vector3 (xPos, yPos, zPos);
 		}
 	}
 	public void DragStart (){
