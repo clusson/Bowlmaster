4fd78c178729c9125af7e8629ed4492c0b844dbb
clusson <clementlusson@gmail.com>
2017-09-11 13:58:25 +0200

    8 - 213

--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -6,16 +6,28 @@ public class ActionMaster {
 
 	public enum Action {Tidy, Reset, EndTurn, EndGame};
 
-	// private int[] bowls = new int[21];
+	private int[] bowls = new int[21];
 	private int bowl = 1;
 
 	public Action Bowl (int pins) {
-	
 		if(pins < 0 || pins > 10) {
 			throw new  UnityException ("Invalid pins!");
 		}
 
-		// Other behaviour here, e.g last frame.
+		bowls [bowl-1] = pins;
+
+		if (bowl == 21){
+			return Action.EndGame;
+		}
+
+		// Handle last-frame special cases
+		if (bowl >=19 && Bowl21Awarded()){
+			bowl += 1;
+			return Action.Reset;
+		} else if (bowl ==20 && !Bowl21Awarded()) {
+			return Action.EndGame;
+		}
+
 		if (pins == 10){
 			bowl += 2;
 			return Action.EndTurn;
@@ -32,5 +44,10 @@ public class ActionMaster {
 		}
 
 		throw new  UnityException ("Not sure what action to return!");
+	}
+
+	private bool Bowl21Awarded() {
+		// Remember that arrays start coutning at 0 
+		return (bowls [19-1] + bowls[20-1] >=10); 
 	}	
 }
